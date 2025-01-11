70f45070208f8f36c706e71fd51973bac2971ad5
Jorge Manrique <georgemanc.28@gmail.com>
2025-01-11 07:46:22 -0500

    Se crea la primera ruta para series

---
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -0,0 +1,12 @@
+package com.aluracursos.screenmatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+    @GetMapping("/series")
+    public String mostrarMensaje(){
+        return "Bienvenido a las mejores series web";
+    }
+}
